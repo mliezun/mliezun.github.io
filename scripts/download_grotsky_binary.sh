90600c91db1f23693bb7aaa39f4f789d5f0453ab
Miguel Liezun <liezun.js@gmail.com>
2025-05-05 14:27:30 -0300

    Update Grotsky binary script to version 0.0.15 and adjust binary references

--- scripts/download_grotsky_binary.sh
+++ scripts/download_grotsky_binary.sh
@@ -4,26 +4,33 @@ set -euo pipefail
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     OS="linux"
+    ARCH="x86_64"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     OS="macos"
+    ARCH="aarch64"
 else
     echo "OS not supported."
     exit 1
 fi
 
-GROTSKY_VERSION=v0.0.14
+GROTSKY_VERSION=v0.0.15
 GROTSKY_RELEASE=${GROTSKY_VERSION}
 
 GROTSKY_FOLDER=.grotsky
 
-GROTSKY_TARGZ=grotsky-$GROTSKY_VERSION-$OS-x86_64.tar.gz
+GROTSKY_TARGZ=grotsky-$GROTSKY_VERSION-$OS-$ARCH.tar.gz
+GROTSKY_BINARY=$GROTSKY_FOLDER/grotsky-rs
 
-if [ ! -f "$GROTSKY_FOLDER/grotsky" ]; then
+if [ ! -f "$GROTSKY_BINARY" ]; then
     mkdir -p $GROTSKY_FOLDER
     echo "Downloading Grotsky Binary..."
     echo "https://github.com/mliezun/grotsky/releases/download/$GROTSKY_RELEASE/$GROTSKY_TARGZ"
     wget "https://github.com/mliezun/grotsky/releases/download/$GROTSKY_RELEASE/$GROTSKY_TARGZ" -P $GROTSKY_FOLDER > /dev/null 2>&1
     tar -xvf $GROTSKY_FOLDER/$GROTSKY_TARGZ -C $GROTSKY_FOLDER > /dev/null 2>&1
+    chmod +x $GROTSKY_BINARY
+    echo "Grotsky Binary downloaded and extracted to $GROTSKY_BINARY"
+    echo "Cleaning up..."
+    # Remove the tar.gz file
     rm -rf $GROTSKY_FOLDER/$GROTSKY_TARGZ
     echo "Done!"
 fi
